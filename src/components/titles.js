1231ba2b2519260f959dfa19b46fcbf6cc8235d5
Rajat Khanna <rajatkhanna311@gmail.com>
2019-05-12 20:05:22 +0200

    React weather app

---
+++ src/components/titles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Titles = (props) => {
+    return (
+        <div>
+            <h1 className="title-container__title">Weather App</h1>
+            <p className="title-container__subtitle">Search for current weather in any city: </p>
+        </div>
+    )
+}
+
+export default Titles;
+
